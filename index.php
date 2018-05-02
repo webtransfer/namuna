b2f9708941697ffb6e02fffd73cdc9c45a9be804
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-05-02 12:17:56 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="Keywords" content="uzhost, whois, домен, доменное имя, регистрация домена, зарегистрировать домен, домен com, домен ru, имя домена, создать домен ru, имя сайта, адрес домена, перенос домена, проверка домена +на занятость, проверить домен, заказ домена, реселлинг доменов, свободный домен, русские домены, международные домены, 0677b85884061f20d22e, Партнёрский договор №1031752/P"/>
+<meta name="Description" content="Регистрация доменных имен по очень сниженным ценам от 89 рублей за домен ru. Описание услуг uzhost.net"/>
+<meta name="author" content="Shahakademia Group">
+<meta name="webmoney" content="E714B5C6-9687-4B6A-BA17-47064D9A00E6"/>
+
+<title>
+UZHOST.NET - Система Регистрации Доменных Имен </title>
+ 
+<script type="text/javascript">
+//<![CDATA[
+try{if (!window.CloudFlare) {var CloudFlare=[{verbose:0,p:0,byc:0,owlid:"cf",bag2:1,mirage2:0,oracle:0,paths:{cloudflare:"/cdn-cgi/nexp/dok3v=1613a3a185/"},atok:"58946ac3a6a77d5fd39886fec7df1908",petok:"39a068b30da979feabed8bfa4ed18d37959e7a3e-1458895513-1800",zone:"uzhost.net",rocket:"a",apps:{"abetterbrowser":{"ie":"6","opera":"30","chrome":"44","safari":"8.0.7","firefox":"40.0.1"}},sha2test:0}];document.write('<script type="text/javascript" src="//ajax.cloudflare.com/cdn-cgi/nexp/dok3v=e982913d31/cloudflare.min.js"><'+'\/script>');}}catch(e){};
+//]]>
+</script>
+<link href="/css/bootstrap.css" rel="stylesheet">
+ 
+<link href="/css/main.css" rel="stylesheet">
+<link href="/css/font-awesome.min.css" rel="stylesheet">
+<script data-rocketsrc="https://code.jquery.com/jquery-1.10.2.min.js" type="text/rocketscript"></script>
+<script data-rocketsrc="/js/chart.js" type="text/rocketscript"></script>
+ 
+<!--[if lt IE 9]>
+      <script src="/assets/js/html5shiv.js"></script>
+      <script src="/assets/js/respond.min.js"></script>
+      
+      <script type="text/javascript" src="/assets/js/jquery-1.4.1.min.js"></script>
+      <script type="text/javascript" src="/assets/js/custom.js"></script>
+      <script type="text/javascript">
+      $(document).ready(function() {
+      	$("body").css("display", "none");
+      });
+      </script>
+      <script type="text/javascript">
+      $(document).ready(function() {
+            $("body").css("display", "none");
+      	    $("body").fadeIn(2000);
+      });
+      </script>
+    <![endif]-->
+</head>
+<body><script type="text/javascript">
+//<![CDATA[
+try{(function(a){var b="https://",c="uzhost.net",d="/cdn-cgi/cl/",e="img.gif",f=new a;f.src=[b,c,d,e].join("")})(Image)}catch(e){}
+//]]>
+</script>
+ 
+<div class="navbar navbar-default navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" href="/"><img src="css/logo.png" alt="uzhost.net"></a>
+</div>
+<div class="navbar-collapse collapse">
+<ul class="nav navbar-nav navbar-right">
+<li class="active"><a href="https://uzhost.net/my/billing.php?do=zones" class="transition">ДОМЕНЫ И ЦЕНЫ</a></li>
+<li><a href="/my/news.php" class="transition">НОВОСТИ</a></li>
+<li><a href="/my/billing.php?do=order&type=d" class="transition">РЕГИСТРАЦИЯ</a></li>
+<li><a href="/my/billing.php" class="transition">ВОЙТИ</a></li>
+</ul>
+</div> 
+</div>
+</div>
+<div class="head_domain_search">
+<div class="container">
+<h2 class="centered">НАЙТИ СВОЙ ​​ИДЕАЛЬНЫЙ ДОМЕН ПРОСТО</h2>
+<p class="centered">Мы рады предоставить Вам самые выгодные тарифы на регистрацию и продление доменных имён.</p>
+<div class="domain-search">
+<form class="form-domain" action="/my/whois.php" method="GET">
+ 
+<div class="row">
+<div class="col-lg-9 col-lg-12">
+<div class="search-input">
+<div class="input-group">
+<span class="input-group-addon" id="basic-addon1">www.</span>
+<input type="text" name="whoisdomain" class="form-control input-domain" placeholder="Введите имя домена и зону" aria-describedby="basic-addon1">
+</div>
+</div> 
+</div>
+<div class="col-lg-3 col-lg-6">
+<button class="input_submit" type="submit"><strong>Проверить Домен</strong></button>
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+<div class="content_domain_list">
+<div class="container">
+<div class="row">
+<div class="content_pages_title centered">
+<h1><strong>Доменные имена и стоимость регистрации/продления</strong></h1>
+<h5>Просим ознакомиться со стоимостью регистрации и продления доменов, а так же с дополнительной информацией по домену.</h5>
+</div>
+<div class="content_domains">
+<div class='table_domain_head_group'>
+<h3>
+<strong>
+Россия и СНГ
+</strong>
+</h3>
+</div><div class='table-responsive'><table class='table'><thead><tr class=table_domain_head align=center>
+    <Td><B>доменная зона</B></td><td class='hidden-xs' style='width: 30%;'><B>описание</B><td><B>регистрация</B></td><td class=hidden-xs><B>продление</B></td><td></td></tr></thead><tbody><tr>
+    <Td align=center><B><a title='Регистрация домена в зоне .kz' href='domain/kz/'>.kz</a></B></td><td class=hidden-xs align=center>"Казахстан"</td><td align=center>1599 руб.</td><td class=hidden-xs align=center>1649 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=kz>Купить Сейчас</a></td></tr><tr>
+    <Td align=center><B><a title='Регистрация домена в зоне .ru' href='domain/ru/'>.ru</a></B></td><td class=hidden-xs align=center>"Российская Федерация"</td><td align=center>199 руб.</td><td class=hidden-xs align=center>199 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=ru>Купить Сейчас</a></td></tr><tr>
+    <Td align=center><B><a title='Регистрация домена в зоне .рф' href='domain/рф/'>.рф</a></B></td><td class=hidden-xs align=center>"Российская Федерация"</td><td align=center>199 руб.</td><td class=hidden-xs align=center>199 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=рф>Купить Сейчас</a></td></tr><tr>
+    <Td align=center><B><a title='Регистрация домена в зоне .su' href='domain/su/'>.su</a></B></td><td class=hidden-xs align=center>"Российская Федерация"</td><td align=center>399 руб.</td><td class=hidden-xs align=center>399 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=su>Купить Сейчас</a></td></tr></table><BR></div> 
+
+<div class='table_domain_head_group'>
+<h3>
+<strong>
+Международные
+</strong>
+</h3>
+</div><div class='table-responsive'><table class='table'><thead><tr class=table_domain_head align=center><Td><B>доменная зона</B></td><td class='hidden-xs' style='width: 30%;'><B>описание</B><td><B>регистрация</B></td><td class=hidden-xs><B>продление</B></td><td></td></tr></thead><tbody><tr>
+    
+    <Td align=center><B><a title='Регистрация домена в зоне .biz' href='domain/biz/'>.biz</a></B></td><td class=hidden-xs align=center>"Бизнес-структуры"</td><td align=center>216 руб.</td><td class=hidden-xs align=center>867 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=biz>Купить Сейчас</a></td></tr><tr>
+        <Td align=center><B><a title='Регистрация домена в зоне .com' href='domain/com/'>.com</a></B></td><td class=hidden-xs align=center>"Коммерческие организации"</td><td align=center>499 руб.</td><td class=hidden-xs align=center>720 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=com>Купить Сейчас</a></td></tr><tr>
+            <Td align=center><B><a title='Регистрация домена в зоне .info' href='domain/info/'>.info</a></B></td><td class=hidden-xs align=center>"Информационные сайты"</td><td align=center>247 руб.</td><td class=hidden-xs align=center>774 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=info>Купить Сейчас</a></td></tr><tr>
+                <Td align=center><B><a title='Регистрация домена в зоне .mobi' href='domain/mobi/'>.mobi</a></B></td><td class=hidden-xs align=center>"Мобильные устройства"</td><td align=center>216 руб.</td><td class=hidden-xs align=center>1115 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=mobi>Купить Сейчас</a></td></tr><tr>
+                    <Td align=center><B><a title='Регистрация домена в зоне .name' href='domain/name/'>.name</a></B></td><td class=hidden-xs align=center>"Персональные сайты"</td><td align=center>543 руб.</td><td class=hidden-xs align=center>699 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=name>Купить Сейчас</a></td></tr><tr>
+                        <Td align=center><B><a title='Регистрация домена в зоне .net' href='domain/net/'>.net</a></B></td><td class=hidden-xs align=center>"Сетевые организации"</td><td align=center>525 руб.</td><td class=hidden-xs align=center>880 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=net>Купить Сейчас</a></td></tr><tr>
+                            <Td align=center><B><a title='Регистрация домена в зоне .org' href='domain/org/'>.org</a></B></td><td class=hidden-xs align=center>"Организации"</td><td align=center>786 руб.</td><td class=hidden-xs align=center>793.1 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=org>Купить Сейчас</a></td></tr><tr>
+                                <Td align=center><B><a title='Регистрация домена в зоне .tv' href='domain/tv/'>.tv</a></B></td><td class=hidden-xs align=center>"Тувалу"</td><td align=center>2000 руб.</td><td class=hidden-xs align=center>2222 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=tv>Купить Сейчас</a></td></tr></table><BR></div> 
+
+<div class='table_domain_head_group'>
+<h3>
+<strong>
+Азия
+</strong>
+</h3>
+</div><div class='table-responsive'><table class='table'><thead><tr class=table_domain_head align=center><Td><B>доменная зона</B></td><td class='hidden-xs' style='width: 30%;'><B>описание</B><td><B>регистрация</B></td><td class=hidden-xs><B>продление</B></td><td></td></tr></thead><tbody><tr>
+    
+    <Td align=center><B><a title='Регистрация домена в зоне .asia' href='domain/asia/'>.asia</a></B></td><td class=hidden-xs align=center>"Азия"</td><td align=center>216 руб.</td><td class=hidden-xs align=center>867 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=asia>Купить Сейчас</a></td></tr><tr>
+        <Td align=center><B><a title='Регистрация домена в зоне .in' href='domain/in/'>.in</a></B></td><td class=hidden-xs align=center>"Индия"</td><td align=center>557 руб.</td><td class=hidden-xs align=center>577 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=in>Купить Сейчас</a></td></tr></table><BR></div> 
+
+
+<div class='table_domain_head_group'>
+<h3>
+<strong>
+Другие страны
+</strong>
+</h3>
+</div><div class='table-responsive'><table class='table'><thead><tr class=table_domain_head align=center><Td><B>доменная зона</B></td><td class='hidden-xs' style='width: 30%;'><B>описание</B><td><B>регистрация</B></td><td class=hidden-xs><B>продление</B></td><td></td></tr></thead><tbody><tr>
+    
+    <Td align=center><B><a title='Регистрация домена в зоне .pw' href='domain/pw/'>.pw</a></B></td><td class=hidden-xs align=center>"Палау"</td><td align=center>70 руб.</td><td class=hidden-xs align=center>633.79 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=pw>Купить Сейчас</a></td></tr><tr>
+        <Td align=center><B><a title='Регистрация домена в зоне .ws' href='domain/ws/'>.ws</a></B></td><td class=hidden-xs align=center>"Веб-сайты"</td><td align=center>1369 руб.</td><td class=hidden-xs align=center>1369 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=ws>Купить Сейчас</a></td></tr></table><BR></div> 
+
+<div class='table_domain_head_group'>
+<h3>
+<strong>
+Новые домены (New gTLD)
+</strong>
+</h3>
+</div><div class='table-responsive'><table class='table'><thead><tr class=table_domain_head align=center><Td><B>доменная зона</B></td><td class='hidden-xs' style='width: 30%;'><B>описание</B><td><B>регистрация</B></td><td class=hidden-xs><B>продление</B></td><td></td></tr></thead><tbody><tr>
+    
+    <Td align=center><B><a title='Регистрация домена в зоне .top' href='domain/top/'>.top</a></B></td><td class=hidden-xs align=center>"Tоп"</td><td align=center>154 руб.</td><td class=hidden-xs align=center>278 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=top>Купить Сейчас</a></td></tr>
+        <Td align=center><B><a title='Регистрация домена в зоне .site' href='domain/site/'>.site</a></B></td><td class=hidden-xs align=center>"Сайт"</td><td align=center>70 руб.</td><td class=hidden-xs align=center>1399 руб.</td><td align=center><a class='btn btn-default btn-sm padding-xs' href=/my/order.php?type=d&zone=site>Купить Сейчас</a></td></tr></table><BR></div>  </div>
+
+</div>
+</div>
+</div>
+<div class="modal fade" id="price" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<h4 class="modal-title" id="myModalLabel">Ценовая Политика</h4>
+</div>
+<div class="modal-body">
+<p>*Цены на русские домены указаны в рублях, цены на зарубежные домены указаны в долларах США, при оплате возможна конвертация валют по текущему банковкому курсу. Цены на домены в рублях фиксированы, цены на домены в долларах США могут менятся в меньшую сторону в зависимости от политики и акций зарубежных регистраторов. Цены на зарубежные домены в рублях могут меняться в зависимости от курса рубля к доллару. На сайте указаны розничные цены для клиентов.</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
+</div>
+</div>
+</div>
+</div>
+<div id="skills">
+<div class="container">
+<div class="row grid">
+<div class="mt"></div>
+<div class="col-sm-6 col-md-3">
+<h3>Пользователям</h3>
+<ul>
+<li><a href="https://uzhost.net/my/billing.php">Панель Управления</a></li>
+<li><a href="https://uzhost.net/my/billing.php?do=feedback">Техническая Поддержка</a></li>
+<li><a href="https://uzhost.net/payment-method">Способы Оплаты</a></li>
+</ul>
+</div>
+<div class="col-sm-6 col-md-3">
+<h3>Информация</h3>
+<ul>
+<li><a href="https://uzhost.net/data-center">Дата Центр</a></li>
+<li><a href="https://uzhost.net/about">О Компании</a></li>
+</ul>
+</div>
+<div class="col-sm-6 col-md-3">
+<h3>Услуги</h3>
+<ul>
+<li><a href="https://uzhost.net/my/order.php?type=d">Регистрация Доменов</a></li>
+<li><a href="https://uzhost.net/rootpanel">Лицензия RootPanel</a></li>
+<li><a href="https://uzhost.net/my/whois">Whois Доменов</a></li>
+</ul>
+</div>
+<div class="col-sm-6 col-md-3">
+<h3>Компаниям</h3>
+<ul>
+<li><a href="https://uzhost.net/my/billing.php?do=feedback">Контакты</a></li>
+<li><a href="https://uzhost.net/my/billing.php?do=testimonials">Отзывы клиентов</a></li>
+<li><a href="https://t.me/uzhostnet">Мы в Telegram</a></li>
+<li><script type="text/rocketscript" data-rocketsrc="//yastatic.net/share/share.js" charset="utf-8"></script><div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="small" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,lj,moikrug,surfingbird" data-yashareTheme="counter"></div></li>
+</ul>
+</div>
+</div> 
+</div> 
+</div> 
+<section id="contact"></section>
+<div id="f">
+<div class="container">
+<div class="row">
+<div class="col-lg-3 col-sm-12">
+<p><a href="/">UZHOST.NET © 2012-2018 г.</a></p>
+</div>
+<div class="col-lg-9 col-sm-12 righted">
+
+
+
+<a href="https://waplog.top/go/6"><img src="https://waplog.top/image/6" alt="WAPLOG.TOP"/></a><br/>
+
+ 
+
+ 
+</div>
+</div>
+</div>
+</div>
+ 
+ 
+<script data-rocketsrc="/assets/js/bootstrap.js" type="text/rocketscript"></script> </body>
+</html>
