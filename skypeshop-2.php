a06adbed08dec8d82cfaab2801d164c25b555669
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-08-13 14:01:38 +0500

    Create skypeshop-2.php

---
+++ skypeshop-2.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8" />
+	<!-- Этот сайт был создан @idFox" (Telegram)  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="O'zbekistonga tel qilish, skypega pul tashlash, skype qo'ng'iroq, Узбекистонга тел килиш, Арзон телефон килиш">
+    <meta name="description" content="Скайп ҳисобини тўлдириш">
+    <meta name="google-site-verification" content="---" />
+    <meta name="author" content="MrShahzodbek">
+    <meta property="og:title" content="Skypeдаги аккаунт ҳисобини тўлдириш"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://www.bepul.ru"/>
+    <meta property="og:image" content="https://www.bepul.ru/cover.jpg"/>
+    <meta property="og:site_name" content="BEPUL"/>
+    <meta property="og:description" content="Skype ҳисобини тўлдириш, Ўзбекистонга арзон қўнғироқ қилиш." />
+    <link href="favicon.ico" rel="icon" type="image/x-icon" />
+    
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Skype akkauntlar | Bepul.Ru</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+</head>
+<body>
+
+<br/>
+<div class="container-fluid">
+    <div class="jumbotron" style="border-radius:50px;">
+  <h1 class="text-center">Скайп учун логин ва пароль сотиб олиш!</h1>
+  <p>Скайп орқали Ўзбекистонга арзон ва жуда сифатли қўнғироқ қилишингиз мумкин. Бу ерда сиз Скайп учун логин ва пароль сотиб олишингиз мумкин. Агар сизда «Skype» дастури ўрнатилмаган бўлса «Play Market» ёки «App Store»дан Skype дастурини юклаб олинг. Ёки дастурни ташқи интернет ресурсларидан «Google» орқали изланг.</p>
+    <h2 class="text-center"><font color="red">Қандай сотиб оламан? ДИҚҚАТ БИЛАН ЎҚИНГ!</font></h2>
+    <p><font color="blue">1-босқич:</font> Саҳифа пастидаги рўйҳатдан ичидаги пул бўйича ўзингизга аккаунт танлаб ўнг тарафидаги «Сотиб олиш» тугмасини босинг.</p>
+    <p><font color="blue">2-босқич:</font> Очилган саҳифага ўзингизнинг электрон почта манзилингизни ёзинг, кейинчалик барча  харидларингизни текширишингиз мумкин булади эмейлингиз оркали.</p>
+    <p><font color="blue">3-босқич:</font> Кейинги саҳифада керакли тулов тизимини танланг ва «Тўлаш» («Оплатить») тугмасини босинг.</p>
+    <p><font color="blue">4-босқич:</font> Агарда юқоридаги босқичларни уддалолмасангиз <a href="#">Бу ерга босиб</a> видеони куришни тавсия этиб қоламиз!</p>
+    <h2 class="text-center">Диққат!</h2>
+    <p>1. Сиз қўнғироқ килмоқчи бўлган абонентга «Скайп» дастури умуман талаб этилмайди, чунки <font color="red">сиз тўғридан-тўғри абонент рақамига қўнғироқ қиласиз!</font></p>
+    <p>2. Аккаунт сотиб олсангиз ва пулини тўласангиз <font color="blue">логин пароль сизга автоматик тарзда берилади.</font> <font color="red">Кутишингизга асло хожат юқ!</font></p>
+    <p>3. Агар сайтда ёки тўлов жараёнида <font color="red">хатолик</font> бўлса, <font color="red">тушунмовчиликлар</font> бўлса телеграмда <a href="https://t.me/MrShahzodbek">@MrShahzodbek</a> га мурожаат қилинг.
+    </p><p>5. <font color="blue">Телеграмдаги</font> <a href="https://t.me/Sarmoya">@Sarmoya </a> каналимизга хам албатта обуна булинг.
+    </p><h2 class="text-center">Маълумот:</h2>
+    <p class="text-center">
+        Нарх <font color="red">1 доллар</font> учун <font color="red">20 рубл</font>дан. 5 доллардан юқори аккаунтлар учун 1 доллар = 15 рубл.
+        <font color="blue">Ҳар бир доллар</font> бу Ўзбекистон абоненти билан <font color="blue">КАМИДА 12 дақиқа</font> алоқа дегани!<br>
+        Тўлов қилаётганингизда системалар ушлаб қоладиган фоизга биз жавоб бермаймиз!<br>
+        Биз сотадиган <font color="red">аккаунтларга 2 соат гарантия берамиз</font> яъни 2 соат ичида аккаунт блокланиб қолса ёки пули ўзидан ўзи тугаб қолса бошқа аккаунт берамиз. 2 соатдан кейин биз жавогар эмасмиз аккантларга. Бу дегани 2 соатда блокланиб қолади деганимас, 1 кун, 1 ҳафта 1 ой ҳам туравериши мумкин. Биз фақат 2 соатга кафолат берамиз.
+    </p>
+    <h3 class="text-center" style="margin-bottom:-15px;">Ҳозирга қадар бизнинг система орқали сотиб олинган аккаунтлар миқдори: <strong>+210$</strong></h3>
+    </div>
+    <div class="alert alert-info"><h2 class="text-center">Қуйидаги рўйҳатдан ўзингизга аккаунт танланг!</h2></div>
+    
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        Қуйидаги рўйҳатдан ўзингизга аккаунт танланг.
+                    </div>
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Ичидаги пул $</th>
+                                            <th>Ҳозир мавжуд</th>
+                                            <th>Нархи</th>
+                                            <th>Танлаш</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr class=""><td>2$-2.99$</td><td>1 та</td><td>40 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2494430&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>3$-3.99$</td><td>4 та</td><td>50 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2498807&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>4$-4.99$</td><td>1 та</td><td>50 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2498811&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>5$-5.99$</td><td>3 та</td><td>50 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp/pay_wm.asp?id_d=2498818&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>6$-6.99$</td><td>3 та</td><td>50 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2498820&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>7$-7.99$</td><td>1 та</td><td>50 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2498823&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>8$-8.99$</td><td>1 та</td><td>60 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500220&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>9$-9.99$</td><td>1 та</td><td>70 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500223&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>10$-10.99$</td><td>3 та</td><td>80 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500226&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>11$-11.99$</td><td>1 та</td><td>80 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500227&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>12$-12.99$</td><td>1 та</td><td>80 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500228&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>13$-13.99$</td><td>1 та</td><td>80 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500229&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>14$-14.99$</td><td>1 та</td><td>80 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500230&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>15$-15.99$</td><td>1 та</td><td>90 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500231&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>16$-19.99$</td><td>1 та</td><td>275 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500233&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>20$-24.99$</td><td>1 та</td><td>310 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500235&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>25$-29.99$</td><td>1 та</td><td>350 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp2/pay_wm.asp?id_d=2500237&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    <tr class=""><td>ПАЙНЕТ 10000 СУМ</td><td>100 та</td><td>95 рубл</td><td> <a class="btn btn-default" href="https://www.oplata.info/asp/pay_wm.asp?id_d=1923885&ai=521363&lang=ru-RU">Сотиб олиш</a></td></tr>
+                                    </tbody>
+                                </table>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+        </div>    
+</div>
+<footer class="container-fluid text-center bg-primary">
+        <div class="copyrights" style="margin-top:20px;">
+            <p>SARMOYA © 2018, Барча ҳуқуқлар ҳимояланган!
+                <br>
+                <span>Муаллиф: SARMOYA GROUP</span></p>
+            <p>
+              <a class="btn btn-default" href="https://t.me/Sarmoya" target="_blank"><i class="fa fa-telegram fa-lg" aria-hidden="true" style="color:#259CD8"></i> BEPUL  PRO GROUP</a>
+              <a class="btn btn-default" href="https://www.youtube.com/channel/UCwoOFJOEJYnG4tXexeoPCeQ?sub_confirmation=1" target="_blank"><i class="fa fa-youtube fa-lg" aria-hidden="true" style="color:#FA1818"></i> BEPUL  PRO</a>
+          </p>
+        </div>
+</footer>
+      <!-- FOOTER SECTION END-->
+    <!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME  -->
+    <!-- CORE JQUERY  -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+	<!-- Этот сайт был создан @idFox" (Telegram)  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
