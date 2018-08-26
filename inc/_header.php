00865884bfbdb6b3e0d14edf26519f211fa36447
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-08-26 17:23:11 +0500

    Update _header.php

--- inc/_header.php
+++ inc/_header.php
@@ -9,24 +9,6 @@ $user_id = $_SESSION["user_id"];
 $db->Query("SELECT * FROM db_users_a, db_users_b WHERE db_users_a.id = db_users_b.id AND db_users_a.id = '$user_id'");
 $prof_data = $db->FetchArray();
 ?>
-
-<html>
-	<head>
-		<title>24Invest.Biz | {!TITLE!}</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
-		<meta name="description" content="{!DESCRIPTION!}">
-		<meta name="keywords" content="{!KEYWORDS!}">
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Exo+2:300,400,500&amp;subset=cyrillic,latin-ext" rel="stylesheet"> 
-		<link href="/css/style.css" rel="stylesheet" type="text/css" />
-		<link href="/css/common.css" rel="stylesheet" type="text/css" />
-		
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="/js/functions.js"></script>
-		<link rel="icon" href="./favicon.png">
-	</head>
-	<body>
 <?
 $usid = $_SESSION["user_id"];
 $db->Query("SELECT * FROM db_users_a WHERE id = '$usid'");
@@ -49,123 +31,176 @@ $db->Query("SELECT
 (SELECT COUNT(*) FROM db_users_a WHERE date_reg > '$tfstats') new_users");
 $stats_data = $db->FetchArray();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="{!DESCRIPTION!}">
+		<meta name="keywords" content="{!KEYWORDS!}">
+		
+		<title>24Invest.Biz | {!TITLE!}</title>
 
-<style>
-#blink {
-	color: #bf4f8f;
-	font-size: 14px;
-}
-</style>
+    <!-- Stylesheets
+    ================================================= -->
+		<link rel="stylesheet" href="/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="/css/style.css" />
+		<link rel="stylesheet" href="/css/ionicons.min.css" />
+    <link rel="stylesheet" href="/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/css/demo.css" />
+    <!--Google Webfont-->
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,300italic,400italic,500,500italic,600,600italic,700' rel='stylesheet' type='text/css'>
+    <!--Favicon-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link rel="icon" href="./favicon.png">
+	</head>
+	<body>
 
-<div class="container fone">
-<header>
-		<div class="header-top">
-			Мы выплатили: <span style="display: inline;"><?=sprintf("%.2f",($stats_data["all_payment"])); ?> РУБ.</span>
-			Старт проекта: <span>01.09.2018 года.</span>		
-		</div>
-		<div class="headerContainer">
-			<div class="headerInner">
-				<a href="/" id="logo"></a>
-				<div class="mainNavRight">
-					<div class="navbar">
-						<div class="navbar-inner">
-							<ul class="nav">
-					<?php if($numnews == $newsus){ ?>
-					<li><a href="/news">Новости</a></li>
+    <!-- Header
+    ================================================= -->
+		<header id="header" class="lazy-load">
+      <nav class="navbar navbar-default navbar-fixed-top menu">
+        <div class="container">
+
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/"><img src="/img/logo.png" alt="logo" /></a>
+          </div>
+
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right main-menu">
+              <?php if($numnews == $newsus){ ?>
+					<li class="dropdown"><a href="/news">Новости</a></li>
 					<?php } else if($numnews > $newsus) { $newsplus = $numnews-$newsus; ?>
-					<li><a href="/news"><font id="blink">Новости</font></a></li>
+					<li class="dropdown"><a href="/news"><font id="blink">Новости</font></a></li>
 					<?php } ?>
-					<li><a href="/about">О нас</a></li>
+                <li class="dropdown"><a href="/about">О нас</a></li>
 
 	<?php if ($_SESSION["user_id"]) : ?>
-		<li><a href="/serfing">Букс</a></li>
+		<li class="dropdown"><a href="/serfing">Букс</a></li>
 	<?php endif;?>
 	<?php if (!$_SESSION["user_id"]) : ?>
-		<li><a href="/serfing_guest">Букс</a></li>
+		<li class="dropdown"><a href="/serfing_guest">Букс</a></li>
 	<?php endif;?>
 
-					<li><a href="/rules">Правила</a></li>
-					<li><a href="/help">Контакты</a></li>
+					<li class="dropdown"><a href="/rules">Правила</a></li>
+					<li class="dropdown"><a href="/help">Контакты</a></li>
 	<?php if ($_SESSION["user_id"]) : ?>
-<li><a class="signup" href="/profile">Аккаунт</a></li>
-<li><a class="login" href="/output">Выход</a></li>
+<li class="dropdown"><a class="signup" href="/profile">Аккаунт</a></li>
+<li class="dropdown"><a class="login" href="/output">Выход</a></li>
 	<?php endif;?>
 	<?php if (!$_SESSION["user_id"]) : ?>
-<li><a  class="signup" href="/signup">Регистрация</a></li>
-<li><a class="login" href=".login" data-toggle="modal">Вход</a></li>
+<li class="dropdown"><a  class="signup" href="/signup">Регистрация</a></li>
+<li class="dropdown"><a class="login" href=".login" data-toggle="modal">Вход</a></li>
 <?php endif;?>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div><!-- end headerContainer -->
+            </ul>
+           
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+      </nav>
+    </header>
+    <!--Header End-->
+      <section id="demo-banner">
+    <div class="container">
+     
+    	<div class="row">
+        <div class="logo"><a href="/"><img src="/images/logo.png" alt="" /></a></div>
+        <h1 class="text-white"> Мы выплатили: </h1><div id="incremental-counter" data-value="<?=sprintf("%.2f",($stats_data["all_payment"])); ?>"></div> <h1> РУБ. </h1> <br>
+            
+        <div class="preview-img">
+         
+        </div>
+      </div>
+     
+    </div>
+  </section>
+
+
+
+
+
+  <div id="page-contents">
+    	<div class="container">
+    		<div class="row">
 
 <?php if($_SERVER['REQUEST_URI']=='/' && $_SERVER['REQUEST_URI']!='/index') {  ?>
-	<div>
-        		<div class="bannerContainer">
-			<div class="bannerInner">
-				<div class="ctn-banner bannerLeft">
+                    <div class="col-lg-12">
+					<div class="feature-item col-md-6">
+					<center><i class="fa fa-bank" style="font-size: 45px;"></i>
 					<h3>ИНВЕСТ ПЛАН</h3>
-					<p> <strong>11%-13%</strong> день <span>|</span> <strong>330%-390% </strong> месяц</p>
+					<h4 style="color: #333;"> <p><strong>11%-13%</strong> день <span>|</span> <strong>330%-390% </strong> месяц</p>
 					<p>Окупаемость за 9 дней</p>
-					<p>Инвестиции от 100 до 50.000 рублей </p>
-				</div>
-				<div class="ctn-banner bannerRight">
-					<h3>БЕЗ РИСКА</h3>
-					<p>Инвестиции на <strong> 30 дней </strong></p>
+					<p>Инвестиции от 100 до 50.000 рублей </p></h4>
+                    </center>
+					</div>
+					<div class="feature-item col-md-6">
+                    <center><i class="fa fa-industry" style="font-size: 45px;"></i>
+						<h3>БЕЗ РИСКА</h3>
+					<h4 style="color: #333;"><p>Инвестиции на <strong> 30 дней </strong></p>
 					<p>Индивидуальные тарифные планы</p>
-					<p>Продуманный маркетинг. </p>
-				</div>
-			</div>
-		</div><!-- end bannerContainer -->
-	</div>
-<div class="balanceInner">
-	<h3>бонус <strong>10 РУБ.</strong> НА БАЛАНС ДЛЯ ПОКУПОК ЗА РЕГИСТРАЦИЮ. </h3>
-	<a href="/signup">УЖЕ РЕГИСТРИРУЮСЬ</a>
+					<p>Продуманный маркетинг. </p></h4>
+                      </center>
+					</div>
+                   
+					</div>
+				
+        		
+				
+			
+
+<div class="col-lg-12">
+	<h3>Бонус <strong>10 РУБ.</strong> НА БАЛАНС ДЛЯ ПОКУПОК ЗА РЕГИСТРАЦИЮ.
+	<a class="btn btn-primary" href="/signup">УЖЕ РЕГИСТРИРУЮСЬ</a> </h3>
+
 </div>
 
-<div class="commissionContainer">
-  <div class="commissionInner">
-    <div class="cms-left">
-      <div class="cms-left-row cms-left-row1">
-        <p>баннеры</p>
-      </div>
-      <div class="cms-left-row cms-left-row2">
-        <p>рефералы</p>
-      </div>
-      <div class="cms-left-row cms-left-row3">
-        <p>заработок</p>
-      </div>
-    </div>
-    <div class="cms-right">
-      <h3>Реферальная программа</h3>
-      <span></span>
-      <h4>1-уровень = 10%, 2-уровень = 5%, 3-уровень = 1%</h4>
-    </div>
-    <div class="clear-fix"> </div>
-  </div>
+
+<div class="col-lg-12">
+      <div class="col-lg-4"><center>
+      <p><img src="/img/baner.png"></p>
+        <h3>Баннеры</h3>
+        </center></div>
+       <div class="col-lg-4"><center>
+        <p><img src="/img/ref.png"></p>
+        <p><h3>Рефералы</h3></p>
+           </center></div>
+       <div class="col-lg-4"><center>
+        <p><img src="/img/money.png"></p>
+        <p><h3>Заработок</h3></p>
+         </center></div>
+         <div class="line-divider"></div>
+         <br><br>
 </div>
+    <div class="col-lg-12">
+    <center><img src="/img/referal.png"><center><br>
+      <h3>Реферальная программа</h3>
+       
+      <span></span>
+      <h3>1-уровень = <font style="color: red">10%</font>, 2-уровень = <font style="color: red">5%</font>, 3-уровень = <font style="color: red">1%</font></h3>
+        <div class="line-divider"></div>
+    </div>
+ 
+
 <?php
  } // Главная страница
 ?>
 
 <?php if($_SERVER['REQUEST_URI']!='/' && $_SERVER['REQUEST_URI']!='/index') {  ?>
 <center>
-	<h2 style="text-transform: uppercase;color: #ffe5fd;">{!TITLE!}</h2><br/><br/>
+	<h2>{!TITLE!}</h2><br/><br/>
 </center>
 <?PHP 
 } // Остальные страницы
  ?>
- 
-</header>
 
-
-<div class="container">
-
-	<div class="row">
 <?PHP
 	include("inc/_menu_left.php"); 
 ?>
 
-	<div class="col-md-12">
